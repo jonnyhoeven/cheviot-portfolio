5845e4a663844cff4483a5ba5f9e80ab6cc7c672
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2023-06-24 09:38:52 +0200

    get auth user data

--- pages/protected.vue
+++ pages/protected.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <div>I'm protected! Session data: {{ data }}</div>
-    <button class="rounded-xl shadow-xl p-2 m-2" @click="signOut()">
-      sign out
-    </button>
+    <div class="flex">
+      Hi there, {{ data?.user?.name }}!
+      <img
+        class="border-2 border-gray-900 rounded-full w-8 h-8  shadow-xl"
+        :src="data?.user?.image?.toString()"
+      >
+      <button class="rounded-xl shadow-xl p-2 m-2" @click="signOut()">
+        sign out
+      </button>
+    </div>
+    <pre>{{ data }}</pre>
   </div>
 </template>
 
