3dcd2357fc8266e81f41f034fa16eb49825dd3c6
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2023-07-06 23:43:30 +0200

    fix label and clean

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -5,21 +5,19 @@ import { useRouter } from 'next/navigation'
 
 export default function LogoutButton() {
   const router = useRouter()
-
-  // Create a Supabase client configured to use cookies
-  const supabase = createClientComponentClient()
+  const supabase = createClientComponentClient();
 
   const signOut = async () => {
-    await supabase.auth.signOut()
-    router.refresh()
-  }
+    await supabase.auth.signOut();
+    router.refresh();
+  };
 
   return (
     <button
       className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
       onClick={signOut}
     >
-      Why are you here?
+      Sign out
     </button>
   );
 }
