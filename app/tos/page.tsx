a7759dd7e4f5b33fd7025ba1e8e6a1921ac1a960
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2023-07-06 23:44:43 +0200

    fix strings

--- app/tos/page.tsx
+++ app/tos/page.tsx
@@ -1,7 +1,5 @@
 export default async function Index() {
   return (
-    <div className="w-full flex flex-col items-center">
-      Terms of service here
-    </div>
+    <div className="w-full flex flex-col items-center">Terms of Service</div>
   );
 }
