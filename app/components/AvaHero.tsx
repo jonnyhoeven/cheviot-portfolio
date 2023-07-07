06919efa25e0b3d7215524d820bb318147f1b91a
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2023-07-07 15:48:12 +0200

    float/breath ava

--- app/components/AvaHero.tsx
+++ app/components/AvaHero.tsx
@@ -4,7 +4,7 @@ export default function AvaHero() {
   return (
     <div className="w-full flex items-center justify-center h-screen dark:text-white">
       <Image
-        className="shadow-xl border-4 border-gray-900 dark:border-gray-100 rounded-full w-32 md:w-64"
+        className="float border-4 border-gray-900 dark:border-gray-100 rounded-full w-32 md:w-64"
         alt="Jonny van der Hoeven"
         src="/jonny.jpg"
         width={400}
