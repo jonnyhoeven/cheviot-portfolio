74b28b5a2cafcd73cd8212f23c63eeed3c264860
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2023-07-07 15:48:46 +0200

    rename/move

--- seed/seed.sql
+++ seed/seed.sql
@@ -1,4 +1,4 @@
-drop table messages;
+/*drop table messages;*/
 create table if not exists messages (
   id uuid default gen_random_uuid() primary key,
   created_at timestamp with time zone default timezone('utc'::text, now()) not null,
