fa94ac8d81b6b05bab8f8c7cb1497d902035c087
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2023-07-07 12:53:30 +0200

    i need orca for these texts

--- app/components/VideoEmbedHero.tsx
+++ app/components/VideoEmbedHero.tsx
@@ -6,7 +6,7 @@ export default function VideoEmbedHero() {
     <div className="w-full grid grid-cols-1 lg:grid-cols-1 gap-10 justify-center px-10 pb-10 bg-gray-500 text-white dark:bg-gray-700">
       <VideoEmbed
         src="https://www.youtube-nocookie.com/embed/videoseries?list=PLFJTjjyvdWZw3sQ3i1lZih1pcmtLKM92i"
-        title="Cool tech and frameworks"
+        title="Sharing is caring:"
       />
     </div>
   );
