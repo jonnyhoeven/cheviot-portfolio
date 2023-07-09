5c26c01502a230cd06166fd3130ee66ab8f411fb
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2023-07-09 13:04:30 +0200

    update schema

--- seed/seed.sql
+++ seed/seed.sql
@@ -1,4 +1,4 @@
-/*drop table messages;*/
+drop table messages;
 create table if not exists messages (
   id uuid default gen_random_uuid() primary key,
   created_at timestamp with time zone default timezone('utc'::text, now()) not null,
@@ -16,10 +16,14 @@ create policy "Users can read their own messages"
 create policy "Users can update their own messages."
   on messages for update using ( auth.uid() = user_id );
 
+drop table posts;
 create table if not exists posts (
   id uuid default gen_random_uuid() primary key,
-  category text default 'post',
+  category text default 'blog',
   title text,
+  slug text,
+  intro text,
+  subtitle text,
   content text,
   image_url text,
   image_alt text,
@@ -35,8 +39,10 @@ alter table posts
 create policy "Everyone can read public posts" on posts
  for select using (is_public = true);
 
-insert into posts (title, content, is_public, category) 
+insert into posts (title, slug, subtitle, intro,  content, is_public, category) 
 values
-('test', 'content', 'true', 'timeline'),
-('test2', 'content2', 'true', 'timeline')
+('test', 'test', 'content', 'intro', 'content', true, 'timeline'),
+('test2', 'test2', 'content2','intro', 'content',  true, 'timeline'),
+('testb', 'testb', 'contentb', 'intro', 'content', true, 'blog'),
+('testb2', 'testb2', 'contentb2','intro', 'content',  true, 'blog')
 ;
