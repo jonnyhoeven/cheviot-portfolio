1a00315df0e03bb2c2a8cc9b53c023154a09950d
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2023-08-05 13:22:46 +0200

    📧

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,19 +12,6 @@ export default function Login() {
   const router = useRouter();
   const supabase = createClientComponentClient();
 
-  const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!email || !password) return new Error("Missing email or password");
-    await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${process.env.NEXT_PUBLIC_SERVER}/auth/callback`,
-      },
-    });
-    setView("check-email");
-  };
-
   const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     await supabase.auth.signInWithPassword({
@@ -65,48 +52,13 @@ export default function Login() {
 
   return (
     <div className="animate-in flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      {view === "check-email" ? (
-        <p className="text-center text-foreground">
-          Check <span className="font-bold">{email}</span> to continue signing
-          up
-        </p>
-      ) : (
-        <form
-          className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-          onSubmit={view === "sign-in" ? handleSignIn : handleSignUp}
-        >
-          <label className="text-md" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
-            name="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            placeholder="you@example.com"
-          />
-          <label className="text-md" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
-            type="password"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            placeholder="••••••••"
-          />
+      <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+        <>
           {view === "sign-in" && (
             <>
-              <button className="flex items-center bg-gray-700 rounded px-4 py-2 text-white mb-6">
-                <div>
-                  <span className="wiggle-in text-2xl mr-16">🍒</span>
-                </div>
-                <div>Sign In with Email verification</div>
-              </button>
               <button
                 onClick={handleGoogleSignIn}
-                className="flex  items-center bg-gray-700 rounded px-4 py-2 text-white mb-6"
+                className="flex items-center bg-gray-300 dark:bg-gray-700 rounded px-4 py-2 text-white mb-6"
               >
                 <div className="mr-16">
                   <svg
@@ -139,7 +91,7 @@ export default function Login() {
               </button>
               <button
                 onClick={handleGitHubSignIn}
-                className="flex  items-center bg-gray-700 rounded px-4 py-2 text-white mb-6"
+                className="flex  items-center  bg-gray-300 dark:bg-gray-700 rounded px-4 py-2 text-white mb-6"
               >
                 <div className="mr-16">
                   <svg
@@ -150,8 +102,8 @@ export default function Login() {
                     aria-hidden="true"
                   >
                     <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
                       viewBox="0 0 28 28"
                       d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
                       fill="#24292f"
@@ -160,35 +112,10 @@ export default function Login() {
                 </div>
                 <div>Sign In with GitHub</div>
               </button>
-              <p className="text-sm text-center">
-                Don't have an account?
-                <button
-                  className="ml-1 underline"
-                  onClick={() => setView("sign-up")}
-                >
-                  Sign Up Now
-                </button>
-              </p>
             </>
           )}
-          {view === "sign-up" && (
-            <>
-              <button className="bg-gray-700 rounded px-4 py-2 text-white mb-6">
-                Sign Up
-              </button>
-              <p className="text-sm text-center">
-                Already have an account?
-                <button
-                  className="ml-1 underline"
-                  onClick={() => setView("sign-in")}
-                >
-                  Sign In Now
-                </button>
-              </p>
-            </>
-          )}
-        </form>
-      )}
+        </>
+      </form>
     </div>
   );
 }
