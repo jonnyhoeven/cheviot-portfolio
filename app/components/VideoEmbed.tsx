be9523114187177506b42c3a7294ba00c6e26d7c
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2023-08-05 12:56:54 +0200

    remove title

--- app/components/VideoEmbed.tsx
+++ app/components/VideoEmbed.tsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function VideoEmbed({ src = "", title = "" }) {
   return (
     <div>
-      <div className="text-2xl pt-10 pb-3">{title}</div>
       <iframe
         className="rounded-xl shadow-xl w-full aspect-video"
         src={src}
--- app/components/VideoEmbedHero.tsx
+++ app/components/VideoEmbedHero.tsx
@@ -3,7 +3,7 @@ import VideoEmbed from "./VideoEmbed";
 
 export default function VideoEmbedHero() {
   return (
-    <div className="w-full grid grid-cols-1 lg:grid-cols-1 gap-10 justify-center px-10 pb-10 bg-gray-500 text-white dark:bg-gray-700">
+    <div className="w-full grid grid-cols-1 lg:grid-cols-1 gap-10 justify-center px-10 py-10 bg-gray-500 text-white dark:bg-gray-700">
       <VideoEmbed
         src="https://www.youtube-nocookie.com/embed/videoseries?list=PLFJTjjyvdWZw3sQ3i1lZih1pcmtLKM92i"
         title="Sharing is caring:"
