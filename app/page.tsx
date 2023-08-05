26ed52dcc5c324d234365b3d704c2d2bd80aa5d8
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2023-08-05 12:56:42 +0200

    remove unused

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import AvaHero from "@/components/AvaHero";
 import VideoEmbedHero from "@/components/VideoEmbedHero";
-import HistoricTimeline from "@/components/HistoricTimeline";
 
 export default async function Index() {
   return (
     <div className="w-full">
       <AvaHero />
-
-      <HistoricTimeline />
 
       <VideoEmbedHero />
     </div>
